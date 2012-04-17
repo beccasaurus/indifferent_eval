4701dfccf4352ebb1ef1aefe0e6510db9953fa53
remi <remi@remitaylor.com>
2012-04-16 22:52:07 -0700

    Supports Ruby 1.8 and Ruby 1.9

--- lib/indifferent_eval/module.rb
+++ lib/indifferent_eval/module.rb
@@ -1,8 +1,10 @@
 module IndifferentEval
 
-  def indifferent_eval object_to_eval_on = self, &block
-    
-    if block.arity == -1
+  def indifferent_eval(object_to_eval_on = self, &block)
+
+    # Blocks with no arguments return -1 in Ruby 1.8 and 0 in Ruby 1.9
+
+    if block.arity == 0 || block.arity == -1
       # no block variable, use instance_eval
       object_to_eval_on.instance_eval &block
 
