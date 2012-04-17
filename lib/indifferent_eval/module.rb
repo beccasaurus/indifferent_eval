1c64578fb8ea5051d54ca247af7c5ac37fde60a5
remi <remi@remitaylor.com>
2012-04-16 23:49:33 -0700

    Squished source code a bit (terse but clear IMHO)

--- lib/indifferent_eval/module.rb
+++ lib/indifferent_eval/module.rb
@@ -1,18 +1,9 @@
 module IndifferentEval
-
   def indifferent_eval(object_to_eval_on = self, &block)
-
-    # Blocks with no arguments return -1 in Ruby 1.8 and 0 in Ruby 1.9
-
-    if block.arity == 0 || block.arity == -1
-      # no block variable, use instance_eval
-      object_to_eval_on.instance_eval &block
-
+    if block.arity == 0 || block.arity == -1 # Blocks with no arguments return -1 in Ruby 1.8 but 0 in Ruby 1.9.
+      object_to_eval_on.instance_eval &block # No block variable, use instance_eval.
     else  
-      # we were passed a variable, so #call the block
-      block.call object_to_eval_on
+      block.call object_to_eval_on # We were passed a variable, so #call the block.
     end
-
   end
-
 end
